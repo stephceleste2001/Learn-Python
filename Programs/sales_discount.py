ccd132e720660308c938a7cbf586c528ea05fb9c
Stephanie Foley <stephceleste2001@gmail.com>
2025-07-05 17:14:13 -0400

    Update README

--- Programs/sales_discount.py
+++ Programs/sales_discount.py
@@ -1,9 +1,9 @@
 # Retrieving user inputs
-itemPrice = float(input("What is the price of the item?"))
+itemPrice = float(input("What is the price of the item? $"))
 percentageDiscount = float(input("What is the percentage discount?"))
 
 # Processing
 reducedPrice = itemPrice * percentageDiscount / 100
 
 # Generting the output
-print("The reduced price is " + str(reducedPrice))
+print("The reduced price is $" + str(reducedPrice))
