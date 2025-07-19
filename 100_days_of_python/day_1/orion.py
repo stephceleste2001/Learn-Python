c89573de9e199e1be0dccc6a7c81013bf1eaabe5
Stephanie Foley <stephceleste2001@gmail.com>
2025-07-19 17:23:50 -0400

    Update README

---
+++ 100_days_of_python/day_1/orion.py
@@ -0,0 +1,18 @@
+# This program draws the stars of the Orion constellation,
+# the names of the stars, and the constellation lines.
+import turtle
+
+# Set the window size.
+turtle.setup(500,600)
+
+# setup the turtle.
+turtle.penup()
+turtle.hideturtle()
+
+# Create named constants for the star coordinates.
+LEFT_SHOULDER_X = -70
+LEFT_SHOULD_Y = 200
+
+RIGHT_SHOULDER_X = 80
+RIGHT_SHOULDER_Y = 180
+
