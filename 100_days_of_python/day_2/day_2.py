b04a5c49c21066a9f4b59896f503c1f5e2cb2547
Stephanie Foley <stephceleste2001@gmail.com>
2025-08-08 15:58:28 -0400

    Update README

---
+++ 100_days_of_python/day_2/day_2.py
@@ -0,0 +1,23 @@
+# Data Types
+
+# 1. String
+
+print("Hello World")
+print('Hello World')
+
+# String Concatenation
+print("Hello" + "World")
+print("Hello" + " " + "World")
+
+# [0] Represents 'H', [1] 'e' and so on...
+print("Hello[0]")
+print("Hello[3]")
+
+# Can't sum using Strings
+print("123" + "342")
+
+# 2. Integer
+
+print(123 + 342)
+
+# To visualize we use commas in between large numbers Like 13,246,421 but in Python we use '_' Underscores Like 13_246_421 so the Compiler assumes 13246421.
